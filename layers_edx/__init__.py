cbff1fc5200426256548ec90a36d664fc61ff721
Gabriel Wolf <gwolf@student.42vienna.com>
2025-12-30 11:10:37 +0000

    chore: format file

--- layers_edx/__init__.py
+++ layers_edx/__init__.py
@@ -9,19 +9,28 @@ llf = list[list[float]]
 lli = list[list[int]]
 
 
-def read_csv(filename: str,
-             value_offset: int = 0, row_offset: int = 0, column_offset: int = 0,
-             nan: float | int | str = 0,
-             conversion: Callable = lambda x: x,
-             dtype: Type[float | int] = float,
-             fill_value: Any = None) -> llf | lli:
+def read_csv(
+    filename: str,
+    value_offset: int = 0,
+    row_offset: int = 0,
+    column_offset: int = 0,
+    nan: float | int | str = 0,
+    conversion: Callable = lambda x: x,
+    dtype: Type[float | int] = float,
+    fill_value: Any = None,
+) -> llf | lli:
     fill_value = [] if fill_value is None else [fill_value]
     values = [fill_value for _ in range(value_offset)]
-    with open(f'{BASE_PATH}/resources/{filename}.csv') as file:
+    with open(f"{BASE_PATH}/resources/{filename}.csv") as file:
         current = -1
         for row in csv.reader(file):
             current += 1
             if row_offset > current:
                 continue
-            values.append([dtype(nan) if value == '' else conversion(dtype(value)) for value in row[column_offset:]])
+            values.append(
+                [
+                    dtype(nan) if value == "" else conversion(dtype(value))
+                    for value in row[column_offset:]
+                ]
+            )
     return values
