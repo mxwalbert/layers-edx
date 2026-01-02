751c18d7381e2b78d37b6675db45176501877558
Gabriel Wolf <gwolf@student.42vienna.com>
2026-01-02 11:29:31 +0000

    chore: format file

--- layers_edx/material_properties/jr.py
+++ layers_edx/material_properties/jr.py
@@ -7,7 +7,8 @@ from layers_edx.units import ToSI
 class JumpRatio:
     """
     Provides algorithms to compute the jump ratio of a given atomic shell,
-    which quantifies the discontinuity in the mass absorption coefficient at the shell’s ionization energy.
+    which quantifies the discontinuity in the mass absorption coefficient at the shell's
+    ionization energy.
     """
 
     class Algorithm(Protocol):
@@ -29,19 +30,21 @@ class JumpRatio:
             ...
 
     class Heinrich1986(Algorithm):
-
         mac = MassAbsorptionCoefficient
         DELTA = ToSI.ev(0.001)
 
         @classmethod
         def compute(cls, shell: AtomicShell) -> float:
             edge_energy = shell.edge_energy
-            return cls.mac.compute(shell.element, edge_energy + cls.DELTA) / cls.mac.compute(shell.element, edge_energy - cls.DELTA)
+            return cls.mac.compute(
+                shell.element, edge_energy + cls.DELTA
+            ) / cls.mac.compute(shell.element, edge_energy - cls.DELTA)
 
     @classmethod
     def compute(cls, shell: AtomicShell) -> float:
         """
-        Computes the jump ratio for a given atomic shell using the default algorithm (Heinrich 1986).
+        Computes the jump ratio for a given atomic shell using the default algorithm
+        (Heinrich 1986).
 
         Args:
             shell (AtomicShell): The atomic shell of interest.
@@ -61,7 +64,8 @@ class JumpRatio:
             shell (AtomicShell): The atomic shell of interest.
 
         Returns:
-            float: The dimensionless ionization fraction, defined as (r - 1) / r for r ≥ 1, otherwise 0.
+            float: The dimensionless ionization fraction, defined as (r - 1) / r
+                for r ≥ 1, otherwise 0.
         """
         r = cls.compute(shell)
         return (r - 1.0) / r if r >= 1.0 else 0.0
