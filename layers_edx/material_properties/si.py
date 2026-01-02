898335d488bef6dc329dcc6a6402e9543ad4409c
Gabriel Wolf <gwolf@student.42vienna.com>
2026-01-02 11:39:22 +0000

    chore: format file

--- layers_edx/material_properties/si.py
+++ layers_edx/material_properties/si.py
@@ -7,10 +7,11 @@ from layers_edx.element import Composition
 
 class SurfaceIonization:
     """
-    Computes the surface ionization correction factor for x-ray generation in electron-excited materials.
+    Computes the surface ionization correction factor for x-ray generation in
+    electron-excited materials.
 
-    The surface ionization factor accounts for the enhancement of x-ray emission due to backscattered electrons
-    near the sample surface.
+    The surface ionization factor accounts for the enhancement of x-ray emission due to
+    backscattered electrons near the sample surface.
     """
 
     class Algorithm(Protocol):
@@ -21,7 +22,9 @@ class SurfaceIonization:
         bc = BackscatterCoefficient
 
         @classmethod
-        def compute(cls, composition: Composition, shell: AtomicShell, energy: float) -> float:
+        def compute(
+            cls, composition: Composition, shell: AtomicShell, energy: float
+        ) -> float:
             """
             Method for computing the surface ionization correction factor.
 
@@ -36,18 +39,22 @@ class SurfaceIonization:
             ...
 
     class Pouchou1991(Algorithm):
-
         @classmethod
-        def compute(cls, composition: Composition, shell: AtomicShell, energy: float) -> float:
+        def compute(
+            cls, composition: Composition, shell: AtomicShell, energy: float
+        ) -> float:
             u0 = energy / shell.edge_energy
             eta = cls.bc.compute(composition, energy)
             r = 2.0 - (2.3 * eta)
             return 1.0 + (3.3 * (1.0 - (1.0 / math.pow(u0, r))) * math.pow(eta, 1.2))
 
     @classmethod
-    def compute(cls, composition: Composition, shell: AtomicShell, energy: float) -> float:
+    def compute(
+        cls, composition: Composition, shell: AtomicShell, energy: float
+    ) -> float:
         """
-        Computes the surface ionization correction factor using the default algorithm (Pouchou1991).
+        Computes the surface ionization correction factor using the default algorithm
+        (Pouchou1991).
 
         Args:
             composition (Composition): The sample composition.
