ef922ded8440a0775e1b3b92dc77334883bbd8d9
Gabriel Wolf <gwolf@student.42vienna.com>
2026-01-02 10:53:39 +0000

    chore: format file

--- layers_edx/material_properties/fym.py
+++ layers_edx/material_properties/fym.py
@@ -10,7 +10,7 @@ class FluorescenceYieldMean:
 
     The fluorescence yield represents the probability that an atom, upon ionization
     of a particular shell, will emit an X-ray photon rather than undergoing
-    an Auger or Coster–Kronig transition.
+    an Auger or Coster-Kronig transition.
     """
 
     class Algorithm(Protocol):
@@ -32,16 +32,26 @@ class FluorescenceYieldMean:
             ...
 
     class Bambynek1972(Algorithm):
-
-        DATA: llf = read_csv('FluorescenceYield', value_offset=1, row_offset=2)
+        DATA: llf = read_csv("FluorescenceYield", value_offset=1, row_offset=2)
 
         @classmethod
         def compute(cls, shell: AtomicShell) -> float:
-            return cls.DATA[shell.element.atomic_number][shell.family] if shell.family <= 2 else 0.0
+            return (
+                cls.DATA[shell.element.atomic_number][shell.family]
+                if shell.family <= 2
+                else 0.0
+            )
 
     class Oz1999(Algorithm):
-
-        DATA: list[float] = [v[0] for v in read_csv('Oz1999', value_offset=Element.from_name('Cu'), row_offset=2, fill_value=0)]
+        DATA: list[float] = [
+            v[0]
+            for v in read_csv(
+                "Oz1999",
+                value_offset=Element.from_name("Cu"),
+                row_offset=2,
+                fill_value=0,
+            )
+        ]
 
         @classmethod
         def compute(cls, shell: AtomicShell) -> float:
@@ -52,8 +62,9 @@ class FluorescenceYieldMean:
         """
         Computes the mean fluorescence yield for a given atomic shell.
 
-        Currently, the method automatically selects the appropriate algorithm based on shell family:
-        - Bambynek1972 for K and L shells (families 0–1),
+        Currently, the method automatically selects the appropriate algorithm based on
+        shell family:
+        - Bambynek1972 for K and L shells (families 0-1),
         - Oz1999 for M shells (family 2).
 
         Args:
