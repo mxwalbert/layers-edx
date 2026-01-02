0292c0b99255590180faebf08dff4c32105a4553
Gabriel Wolf <gwolf@student.42vienna.com>
2026-01-02 10:42:22 +0000

    chore: format file

--- layers_edx/material_properties/bc.py
+++ layers_edx/material_properties/bc.py
@@ -5,11 +5,12 @@ from layers_edx.element import Composition
 
 class BackscatterCoefficient:
     """
-    Provides methods to compute the electron backscatter coefficient for a given material composition
-    and incident electron energy using empirical or theoretical algorithms.
+    Provides methods to compute the electron backscatter coefficient for a given
+    material composition and incident electron energy using empirical or
+    theoretical algorithms.
 
-    The backscatter coefficient quantifies the fraction of incident electrons that are elastically
-    scattered back out of the material.
+    The backscatter coefficient quantifies the fraction of incident electrons that are
+    elastically scattered back out of the material.
     """
 
     class Algorithm(Protocol):
@@ -23,7 +24,8 @@ class BackscatterCoefficient:
             Method for computing the electron backscatter coefficient.
 
             Args:
-                composition (Composition): The elemental composition of the target material.
+                composition (Composition): The elemental composition of the target
+                    material.
                 energy (float): The incident electron energy (J).
 
             Returns:
@@ -32,18 +34,29 @@ class BackscatterCoefficient:
             ...
 
     class PouchouAndPichoir1991(Algorithm):
-
         @classmethod
         def compute(cls, composition: Composition, energy: float) -> float:
-            total = composition.sum_weight_fractions if composition.sum_weight_fractions < 1.1 else 1.1
-            zb = sum([f / total * math.sqrt(e.atomic_number) for e, f in composition.raw_weight_fractions.items()])
+            total = (
+                composition.sum_weight_fractions
+                if composition.sum_weight_fractions < 1.1
+                else 1.1
+            )
+            zb = sum(
+                [
+                    f / total * math.sqrt(e.atomic_number)
+                    for e, f in composition.raw_weight_fractions.items()
+                ]
+            )
             zb *= zb
-            return (1.75e-3 * zb) + (0.37 * (1.0 - math.exp(-0.015 * math.pow(zb, 1.3))))
+            return (1.75e-3 * zb) + (
+                0.37 * (1.0 - math.exp(-0.015 * math.pow(zb, 1.3)))
+            )
 
     @classmethod
     def compute(cls, composition: Composition, energy: float) -> float:
         """
-        Computes the backscatter coefficient for the given composition and incident energy.
+        Computes the backscatter coefficient for the given composition and incident
+        energy.
 
         Currently uses the Pouchou and Pichoir (1991) model.
 
