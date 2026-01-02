fc666516aaaac4f7141a13e3d33cd4a4cf88b8e4
Gabriel Wolf <gwolf@student.42vienna.com>
2026-01-02 10:35:58 +0000

    chore: format file

--- layers_edx/fitting/model_fit/model_fit.py
+++ layers_edx/fitting/model_fit/model_fit.py
@@ -9,10 +9,11 @@ from layers_edx.xrt import XRayTransition
 
 
 class ModelFit(LinearFit):
-
-    def __init__(self,
-                 references: dict[RegionOfInterest, ReferenceModel],
-                 culling_strategy: TCullingStrategy = None):
+    def __init__(
+        self,
+        references: dict[RegionOfInterest, ReferenceModel],
+        culling_strategy: TCullingStrategy = None,
+    ):
         super().__init__(references, culling_strategy)
 
     def _assign_parameters(self) -> None:
@@ -20,11 +21,19 @@ class ModelFit(LinearFit):
         for roi, reference in self.references.items():
             element = roi.first_element
             xrt_set = roi.xrt_set(element)
-            intensities = {xrt: intensity for xrt, intensity in reference.model.items() if xrt in xrt_set.xrts}
+            intensities = {
+                xrt: intensity
+                for xrt, intensity in reference.model.items()
+                if xrt in xrt_set.xrts
+            }
             self._parameters.append(ModelParameter(element, xrt_set, intensities))
 
-    def features_and_targets(self, unknown: dict[XRayTransition, float], selected: list[ModelParameter]) -> tuple[npt.NDArray[np.floating], npt.NDArray[np.floating]]:
+    def features_and_targets(
+        self, unknown: dict[XRayTransition, float], selected: list[ModelParameter]
+    ) -> tuple[npt.NDArray[np.floating], npt.NDArray[np.floating]]:
         features = []
         for p in selected:
-            features.append([p.intensities[xrt] if xrt in p.intensities else 0.0 for xrt in unknown])
+            features.append(
+                [p.intensities[xrt] if xrt in p.intensities else 0.0 for xrt in unknown]
+            )
         return np.array(features), np.array(list(unknown.values())).reshape(-1, 1)
